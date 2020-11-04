15d5b5cadf84e54bec5318c11086c4bdb9b199d8
dennis kabugua <sirkabugua@outlook.com>
2020-11-04 18:29:01 +0300

    Added Command Model to API Project

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -2,14 +2,14 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 namespace CommandAPI.Controllers
 {
-[Route("api/[controller]")]
-[ApiController]
-public class CommandsController : ControllerBase
-{
-[HttpGet]
-public ActionResult<IEnumerable<string>> Get()
-{
-return new string[] {"this", "is", "hard", "coded"};
-}
-}
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "this", "is", "hard", "coded" };
+        }
+    }
 }
